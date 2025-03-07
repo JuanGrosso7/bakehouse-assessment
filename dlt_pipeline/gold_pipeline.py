c41359616f162cd6db7647a953300382778d69a6
Juan Pablo Godoy <juan.godoy@LAT02328.local>
2025-03-06 22:19:18 -0300

    Updated comments and dlt reference

--- dlt_pipeline/gold_pipeline.py
+++ dlt_pipeline/gold_pipeline.py
@@ -1,12 +1,9 @@
-# dlt_pipelines/gold_pipeline.py
 import dlt
 from pyspark.sql import functions as F
 
 @dlt.table(comment="Pivoted transactions per product aggregated by total sales per day")
 def transactions_per_product():
-    # Read transactions from the Silver layer (jp_assessment.latam_lab_silver)
     df = spark.table("jp_assessment.latam_lab_silver.sales_transactions")
-    # Convert dateTime to date for grouping
     df = df.withColumn("trans_date", F.to_date("dateTime"))
     # Pivot: for each day, sum totalPrice per product
     df_pivot = df.groupBy("trans_date").pivot("product").agg(F.sum("totalPrice").alias("total_sales"))
@@ -14,7 +11,6 @@ def transactions_per_product():
 
 @dlt.table(comment="Detailed transactions enriched with customer info")
 def transactions_details():
-    # Read transactions and customer data from Silver layer
     df_trans = spark.table("jp_assessment.latam_lab_silver.sales_transactions")
     df_customers = spark.table("jp_assessment.latam_lab_silver.sales_customers")
     # Join on customerID to add customer details (name, address, email)
